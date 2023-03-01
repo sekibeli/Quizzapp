19779e455790dd6c8355678d54aaf5469ee30d8f
Julia Georgiew <java2011@me.com>
2023-03-01 18:35:17 +0100

    prevent more clicks

--- script.js
+++ script.js
@@ -4,25 +4,24 @@ let progressBar = 0
 
 
 function showContent() {
-    if(currentQuestion >= questions.length){
-               document.getElementById('score').style = '';
-               document.getElementById('cardbox').style = 'display:none';
-               document.getElementById('evaluation-first').innerHTML = `Du hast <b>${correct}</b> von <b>${questions.length}</b> Fragen richtig beantwortet.`;
-              
-        
+    if (currentQuestion >= questions.length) {
+        document.getElementById('score').style = '';
+        document.getElementById('cardbox').style = 'display:none';
+        document.getElementById('evaluation-first').innerHTML = `Du hast <b>${correct}</b> von <b>${questions.length}</b> Fragen richtig beantwortet.`;
     }
     else {
-       
-    document.getElementById('length').innerHTML = questions.length;
-    progress();
-    let question = questions[currentQuestion];
 
-    document.getElementById('question').innerHTML = question.question;
-    document.getElementById('answer_1').innerHTML = question.answer_1;
-    document.getElementById('answer_2').innerHTML = question.answer_2;
-    document.getElementById('answer_3').innerHTML = question.answer_3;
-    document.getElementById('answer_4').innerHTML = question.answer_4;
-    document.getElementById('next-question').disabled = true;
+        document.getElementById('length').innerHTML = questions.length;
+        progress();
+        let question = questions[currentQuestion];
+
+        document.getElementById('question').innerHTML = question.question;
+        document.getElementById('answer_1').innerHTML = question.answer_1;
+        document.getElementById('answer_2').innerHTML = question.answer_2;
+        document.getElementById('answer_3').innerHTML = question.answer_3;
+        document.getElementById('answer_4').innerHTML = question.answer_4;
+        document.getElementById('next-question').disabled = true;
+        switchIt('auto');
     }
 }
 
@@ -41,48 +40,43 @@ function answer(answer) {
     else {
         document.getElementById(`answer_${lastNumber}`).parentNode.classList.add('bg-danger');
         document.getElementById(rightAnswer).parentNode.classList.add('bg-success');
-      
     }
     document.getElementById('next-question').disabled = false;
- 
+    switchIt('none');
 }
 
+
 function nextQuestion() {
-    progressBar = (currentQuestion+1) / questions.length * 100;
+    progressBar = (currentQuestion + 1) / questions.length * 100;
     document.getElementById('progress').style = `width: ${progressBar}%`;
     document.getElementById('progress').innerHTML = `${progressBar}%`;
     currentQuestion++;
     resetColor()
     showContent();
-
-   
 }
 
-function resetColor(){
-    for( let i = 1; i< 5; i++){
+
+function resetColor() {
+    for (let i = 1; i < 5; i++) {
         document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-danger');
         document.getElementById(`answer_${i}`).parentNode.classList.remove('bg-success');
-        }
+    }
 }
 
-function progress(){
-    document.getElementById('current').innerHTML = currentQuestion +1;
+
+function progress() {
+    document.getElementById('current').innerHTML = currentQuestion + 1;
 }
 
-// function showEndScreen(){
-//     document.getElementById('cardbox').innerHTML = endScreenHTML();
-//     currentQuestion = 0;
-// }
 
-
-function switchIt(param){
-    for( let i = 1; i< 5; i++){
-        document.getElementById(`answer_${i}`).parentNode.onclick = param;
-     
-        }
+function switchIt(param) {
+    for (let i = 1; i < 5; i++) {
+        document.getElementById(`answer_${i}`).style.pointerEvents = param;
+    }
 }
 
-function restartGame(){
+
+function restartGame() {
     currentQuestion = 0;
     correct = 0;
     document.getElementById('score').style = "display: none";
